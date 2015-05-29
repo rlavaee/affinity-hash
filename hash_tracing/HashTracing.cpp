6f13c7d4e4f1e0e5f806e7d43aca27d920685dc3
rlavaee <rlavaee@cs.rochester.edu>
2015-05-29 14:16:29 -0400

    changes to hash tracing

--- hash_tracing/HashTracing.cpp
+++ hash_tracing/HashTracing.cpp
@@ -16,6 +16,7 @@
 std::ofstream err("debug.out");
 bool debug = false;
 bool print = false;
+bool layout = true;
 
 
 /*
@@ -28,6 +29,7 @@ const uint32_t analysis_stage_time = 1 << 12;
 entry_vec_t analysis_vec;
 entry_set_t analysis_set;
 entry_set_t excluded_set;
+entry_set_t global_remove_set;
 
 
 
@@ -61,65 +63,74 @@ window_iterator_map_t::iterator window_found;
 /*
  * Saves the affinity data into a file
  */
-void save_affinity_into_file(const char * affinity_file_path) {
-    std::ofstream aff_file(affinity_file_path);
-    aff_file << "max window size: "<< max_fpdist << "\n";
-    aff_file << affinity_map;
+void save_affinity_into_file(const char * affinity_file_path)
+{
+  std::ofstream aff_file(affinity_file_path);
+  aff_file << "max window size: "<< max_fpdist << "\n";
+  aff_file << affinity_map;
 }
 
 
 
 
 
-void affinity_at_exit_handler() {
-    if(print)
-        save_affinity_into_file("graph.babc");
+void affinity_at_exit_handler()
+{
+  if(print)
+    save_affinity_into_file("graph.babc");
+
+  if(layout)
+    find_affinity_layout();
 }
 
 
 
-void dump_window_list (std::ostream &out, window_list_t &wlist) {
+void dump_window_list (std::ostream &out, window_list_t &wlist)
+{
 
-    out << "trace list:------------------------------------------\n" ;
-    out << "size: " << window_list_size << "\n";
-    out << wlist ;
-    out << "---------------------------------------------\n";
+  out << "trace list:------------------------------------------\n" ;
+  out << "size: " << window_list_size << "\n";
+  out << wlist ;
+  out << "---------------------------------------------\n";
 }
 
 
 
-extern "C" void init_affinity_analysis() {
-    window_list_size=0;
-    srand(time(NULL));
+extern "C" void init_affinity_analysis()
+{
+  window_list_size=0;
+  srand(time(NULL));
 
-    // get prepared for the first analysis_set_sampling round
-    analysis_set_sampling = true;
-    count_down = analysis_sampling_time;
-    atexit(affinity_at_exit_handler);
+  // get prepared for the first analysis_set_sampling round
+  analysis_set_sampling = true;
+  count_down = analysis_sampling_time;
+  atexit(affinity_at_exit_handler);
 
-    const char *e = getenv("ST_HASH_DEBUG");
+  const char *e = getenv("ST_HASH_DEBUG");
 
-    if(e && *e) { /*Set and not empty*/
-        debug = true;
-    }
+  if(e && *e)   /*Set and not empty*/
+  {
+    debug = true;
+  }
 
-    e = getenv("ST_HASH_PRINT");
+  e = getenv("ST_HASH_PRINT");
 
-    if(e && *e) { /*Set and not empty*/
-        print = true;
-    }
+  if(e && *e)   /*Set and not empty*/
+  {
+    print = true;
+  }
 
-    if((e = getenv("ST_HASH_ANALYSIS_SIZE")))
-        analysis_set_size = atoi(e);
-    else /* default value; just to make sure it will be working for more than 1*/
-        analysis_set_size = 2;
+  if((e = getenv("ST_HASH_ANALYSIS_SIZE")))
+    analysis_set_size = atoi(e);
+  else /* default value; just to make sure it will be working for more than 1*/
+    analysis_set_size = 2;
 
-    if((e = getenv("ST_HASH_MAX_FPDIST_IND")))
-        max_fpdist_ind = atoi(e);
-    else /* default value; just something non-trivial */
-        max_fpdist_ind = 10;
+  if((e = getenv("ST_HASH_MAX_FPDIST_IND")))
+    max_fpdist_ind = atoi(e);
+  else /* default value; just something non-trivial */
+    max_fpdist_ind = 10;
 
-    max_fpdist = 1 << max_fpdist_ind;
+  max_fpdist = 1 << max_fpdist_ind;
 
 
 }
@@ -151,208 +162,264 @@ extern "C" void init_affinity_analysis() {
    }
    */
 
-void update_affinity(const entry_vec_t& entry_vec, const hash_t& entry, int fpdist_ind) {
-    for(const auto &a_entry : entry_vec) {
-        wcount_t& ref = affinity_map[a_entry].wcount_map[entry];
-        ref.common_windows.at(fpdist_ind)++;
-        ref.all_windows++;
-    }
+void update_affinity(const entry_vec_t& entry_vec, const hash_t& entry, int fpdist_ind)
+{
+  for(const auto &a_entry : entry_vec)
+  {
+    wcount_t& ref = affinity_map[a_entry].wcount_map[entry];
+    ref.common_windows.at(fpdist_ind)++;
+    ref.all_windows++;
+  }
 }
 
 
 
-void add_compress_update(const hash_t& entry, bool analysis) {
+void add_compress_update(const hash_t& entry, bool analysis)
+{
 
 
-    window_list_t::iterator window_it = window_list.begin();
+  window_list_t::iterator window_it = window_list.begin();
 
-    if(analysis) {
-        window_list.emplace_front(entry); /* remember to set the capacity as well */
-        auto res = affinity_map.emplace(entry,0);
-        res.first->second.potential_windows++;
-    }
+  if(analysis)
+  {
+    window_list.emplace_front(entry); /* remember to set the capacity as well */
+    auto res = affinity_map.emplace(entry,0);
+    res.first->second.potential_windows++;
+  }
 
-    if(window_it == window_list.end())
-        return;
+  if(window_it == window_list.end())
+    return;
 
-    auto res = timestamp_map.emplace(entry,timestamp);
+  auto res = timestamp_map.emplace(entry,timestamp);
 
-    int entry_ts = -1;
-    if(!res.second) {
-        entry_ts = res.first->second;
-        res.first->second = timestamp;
-    }
+  int entry_ts = -1;
+  if(!res.second)
+  {
+    entry_ts = res.first->second;
+    res.first->second = timestamp;
+  }
 
-    if(window_it->start <= entry_ts)
-        return;
+  if(window_it->start <= entry_ts)
+    return;
 
-    wsize_t fpdist;
-    if(analysis) {
-        fpdist = 1;
-    } else {
-        window_it->wsize++;
-        fpdist = 0;
-    }
+  wsize_t fpdist;
+  if(analysis)
+  {
+    fpdist = 1;
+  }
+  else
+  {
+    window_it->wsize++;
+    fpdist = 0;
+  }
 
 
-    window_list_t::iterator prev_window_it;
-    wsize_t exp_fpdist = 1;
-    int exp_fpdist_ind = 0;
+  window_list_t::iterator prev_window_it;
+  wsize_t exp_fpdist = 1;
+  int exp_fpdist_ind = 0;
 
-    while(true) {
+  while(true)
+  {
 
-        prev_window_it = window_it;
-        window_it++;
+    prev_window_it = window_it;
+    window_it++;
 
-        prev_window_it->capacity = fpdist+1;
+    prev_window_it->capacity = fpdist+1;
 
 compress:
 
-        if(window_it == window_list.end()) {
-            fpdist += prev_window_it->wsize;
+    if(window_it == window_list.end())
+    {
+      fpdist += prev_window_it->wsize;
 
-            if(fpdist > max_fpdist)
-                return;
-
-            while(fpdist > exp_fpdist) {
-                exp_fpdist <<= 1;
-                exp_fpdist_ind++;
-            }
-            update_affinity(prev_window_it->owners,entry,exp_fpdist_ind);
-            return;
-        }
-
-
-        if(window_it->start <= entry_ts) {
-            window_it->wsize--;
-        }
-
-        if(fpdist+1 >= prev_window_it->wsize + window_it->wsize) {   /* fpdist+1 = prev_window_it->capacity */
-            prev_window_it->wsize += window_it->wsize;
-
-            prev_window_it->merge_owners(*window_it);
-            prev_window_it->start = window_it->start;
-
-            /* may leak memory */
-            window_it = window_list.erase(window_it);
-            goto compress;
-        } else {
-            fpdist += prev_window_it->wsize;
-
-            if(fpdist > max_fpdist)
-                return;
-
-            while(fpdist > exp_fpdist) {
-                exp_fpdist <<= 1;
-                exp_fpdist_ind++;
-            }
-            update_affinity(prev_window_it->owners,entry,exp_fpdist_ind);
-        }
-
-        if(prev_window_it->start <= entry_ts) /* nothing to be done after this */
-            return;
-
-
-    }
-}
-
-
-extern "C" void trace_hash_access(ah_table * tbl, hash_object hash_val, unsigned offset, bool analysis_bit) {
-
-    /*
-     * Count down and transit into the next stage if count drops to zero.
-     */
-// return;
-
-    int r=rand();
-    if((r & sampleMask)!=0)
+      if(fpdist > max_fpdist)
         return;
 
-
-    timestamp++;
-
-    if(!count_down--) {
-
-        if(analysis_set_sampling) {
-            if(debug) {
-                err << "End of the sampling stage, analyzed entries are:\n";
-                for(const auto &a_entry : analysis_vec)
-                    err << a_entry << "\n";
-                err << "\n";
-            }
-            analysis_set_sampling = false;
-            /*
-             * for up to few hundred elements, lookup in vector is faster than in red-black tree:
-             * 	http://cppwisdom.quora.com/std-map-and-std-set-considered-harmful
-             *  analysis_set = entry_set_t(analysis_vec.begin(),analysis_vec.end());
-             */
-            for (const auto &e: analysis_vec)
-                ah_set_analysis_bit(e.table_ptr,e.hash_val,e.offset,1);
-            count_down = analysis_stage_time;
-        } else {
-            if(debug) {
-                err << "Analysis stage finishes now!\n";
-                err.flush();
-            }
-
-            /*
-             * Clear the exluded entry set and the analysis entry set
-             analysis_set.clear();
-             */
-            analysis_vec.clear();
-
-            window_list.clear();
-            timestamp_map.clear();
-
-            for(const auto &e: analysis_vec)
-                ah_set_analysis_bit(e.table_ptr,e.hash_val,e.offset,0);
-
-            /*excluded_set.clear();*/
-
-            /*
-             * Get ready for the next analysis_set_sampling round
-             */
-            analysis_set_sampling = true;
-            count_down = analysis_sampling_time;
-        }
-
-    } else {
-
-
-
-        hash_t entry(tbl,hash_val,offset);
-
-        if(analysis_set_sampling) {
-
-            /*
-             * Fill in the analysis vector first
-             */
-            if(analysis_vec.size() < analysis_set_size)
-                analysis_vec.push_back(entry);
-            else {
-                /*
-                 * reservoir sampling
-                 */
-                int r = rand() % (analysis_sampling_time - count_down);
-                if(r < analysis_set_size)
-                    analysis_vec[r] = entry;
-            }
-        } else {
-            if(debug)
-                dump_window_list(err,window_list);
-
-            add_compress_update(entry,analysis_bit);
-        }
+      while(fpdist > exp_fpdist)
+      {
+        exp_fpdist <<= 1;
+        exp_fpdist_ind++;
+      }
+      update_affinity(prev_window_it->owners,entry,exp_fpdist_ind);
+      return;
     }
+
+
+    if(window_it->start <= entry_ts)
+    {
+      window_it->wsize--;
+    }
+
+    if(fpdist+1 >= prev_window_it->wsize + window_it->wsize)     /* fpdist+1 = prev_window_it->capacity */
+    {
+      prev_window_it->wsize += window_it->wsize;
+
+      prev_window_it->merge_owners(*window_it);
+      prev_window_it->start = window_it->start;
+
+      /* may leak memory */
+      window_it = window_list.erase(window_it);
+      goto compress;
+    }
+    else
+    {
+      fpdist += prev_window_it->wsize;
+
+      if(fpdist > max_fpdist)
+        return;
+
+      while(fpdist > exp_fpdist)
+      {
+        exp_fpdist <<= 1;
+        exp_fpdist_ind++;
+      }
+      update_affinity(prev_window_it->owners,entry,exp_fpdist_ind);
+    }
+
+    if(prev_window_it->start <= entry_ts) /* nothing to be done after this */
+      return;
+
+
+  }
+}
+
+
+extern "C" void trace_hash_access(ah_table * tbl, int object_id, bool analysis_bit)
+{
+
+  /*
+   * Count down and transit into the next stage if count drops to zero.
+   */
+// return;
+
+  int r=rand();
+  if((r & sampleMask)!=0)
+    return;
+
+
+  timestamp++;
+
+  if(!count_down--)
+  {
+
+    if(analysis_set_sampling)
+    {
+      if(debug)
+      {
+        err << "End of the sampling stage, analyzed entries are:\n";
+        for(const auto &a_entry : analysis_vec)
+          err << a_entry << "\n";
+        err << "\n";
+      }
+      analysis_set_sampling = false;
+      /*
+       * for up to few hundred elements, lookup in vector is faster than in red-black tree:
+       * 	http://cppwisdom.quora.com/std-map-and-std-set-considered-harmful
+       *  analysis_set = entry_set_t(analysis_vec.begin(),analysis_vec.end());
+       */
+      for (const auto &e: analysis_vec)
+        ah_set_analysis_bit(e.tbl,e.object_id,1);
+      count_down = analysis_stage_time;
+    }
+    else
+    {
+      if(debug)
+      {
+        err << "Analysis stage finishes now!\n";
+        err.flush();
+      }
+
+      /*
+       * Clear the exluded entry set and the analysis entry set
+       analysis_set.clear();
+       */
+      analysis_vec.clear();
+
+      window_list.clear();
+      timestamp_map.clear();
+
+      for(const auto &e: analysis_vec)
+        ah_set_analysis_bit(e.tbl,e.object_id,0);
+
+      /*excluded_set.clear();*/
+
+      /*
+       * Get ready for the next analysis_set_sampling round
+       */
+      analysis_set_sampling = true;
+      count_down = analysis_sampling_time;
+    }
+
+  }
+  else
+  {
+
+
+
+    hash_t entry(tbl,object_id);
+
+    if(analysis_set_sampling)
+    {
+
+      /*
+       * Fill in the analysis vector first
+       */
+      if(analysis_vec.size() < analysis_set_size)
+        analysis_vec.push_back(entry);
+      else
+      {
+        /*
+         * reservoir sampling
+         */
+        int r = rand() % (analysis_sampling_time - count_down);
+        if(r < analysis_set_size)
+          analysis_vec[r] = entry;
+      }
+    }
+    else
+    {
+      if(debug)
+        dump_window_list(err,window_list);
+
+      add_compress_update(entry,analysis_bit);
+    }
+  }
 
 }
 
-void remove_table_analysis(ah_table * tbl) {
-    entry_vec_t::iterator it=analysis_vec.begin();
-    while(it!=analysis_vec.end()) {
-        if(it->table_ptr==tbl)
-            it=analysis_vec.erase(it);
-        else
-            it++;
+
+
+void remove_table_analysis(ah_table * tbl)
+{
+  entry_vec_t::iterator it=analysis_vec.begin();
+  while(it!=analysis_vec.end())
+  {
+    if(it->table_ptr==tbl)
+      it=analysis_vec.erase(it);
+    else
+      it++;
+  }
+}
+
+void remove_entry(ah_table * tbl, int object_id)
+{
+  global_remove_set.push_back(hash_t(tbl,object_id));
+}
+
+void find_affinity_layout(){
+  std::vector<wcount_pair_t> all_wcount_pairs;
+
+  for(const auto& all_wcount_pair: affinity_map){
+    auto le = all_wcount_pair.first;
+    auto& all_wcount = all_wcount_pair.second;
+    for(const auto& wcount_pair: all_wcount.wcount_map){
+      auto re = wcount_pair.first;
+      uint32_t affinity = wcount_pair.second.affinity();
+      all_wcount_pairs.push_back(affinity_pair(le,re,affinity));
     }
+  }
+
+  std::sort(all_wcount_pairs.begin(),all_wcount_pairs.end());
+  for(const auto& wcount_pair: all_wcount_pairs)
+    err << wcount_pair << "\n";
 }
--- hash_tracing/HashTracing.hpp
+++ hash_tracing/HashTracing.hpp
@@ -29,51 +29,52 @@ uint16_t analysis_set_size;
 uint32_t timestamp = 0;
 
 //affinity representation of a hash table entry
-struct hash_t {
-    ah_table *table_ptr;
+struct hash_t
+{
+  ah_table *table_ptr;
 
-    hash_object hash_val;
+  int object_id;
 
-    unsigned offset;
+  hash_t() {};
 
-    hash_t() {};
+  hash_t(ah_table *a, int b): table_ptr(a), object_id(b), {};
 
-    hash_t(ah_table *a, hash_object b, unsigned c): table_ptr(a),hash_val(b), offset(c) {};
+  hash_t(const hash_t &hentry): table_ptr(hentry.table_ptr), object_id(hentry.object_id) {};
 
-    hash_t(const hash_t &hentry): table_ptr(hentry.table_ptr), hash_val(hentry.hash_val), offset(hentry.offset) {};
+  bool operator == (const hash_t &rhs) const
+  {
+    return table_ptr==rhs.table_ptr && object_id==rhs.object_id;
+  }
 
-    bool operator == (const hash_t &rhs) const {
-        return table_ptr==rhs.table_ptr && hash_val==rhs.hash_val && offset == rhs.offset;
-    }
+  bool operator < (const hash_t &rhs) const
+  {
+    if(table_ptr < rhs.table_ptr)
+      return true;
+    else if(table_ptr > rhs.table_ptr)
+      return false;
 
-    bool operator < (const hash_t &rhs) const {
-        if(table_ptr < rhs.table_ptr)
-            return true;
-        else if(table_ptr > rhs.table_ptr)
-            return false;
+    if(object_id < rhs.object_id)
+      return true;
 
-        if(hash_val < rhs.hash_val)
-            return true;
-        else if(hash_val > rhs.hash_val)
-            return false;
+    return false;
 
-        return (offset < rhs.offset);
-    }
+  }
 
-    friend std::ostream& operator << (std::ostream& out, const hash_t& obj) {
-        out << "(" << std::setbase(16) << obj.table_ptr << ", " << obj.hash_val << ", " << obj.offset << ")" << std::setbase(10);
-        return out;
-    }
+  friend std::ostream& operator << (std::ostream& out, const hash_t& obj)
+  {
+    out << "(" << std::setbase(16) << obj.table_ptr << ", " << obj.object_id << ")" << std::setbase(10);
+    return out;
+  }
 };
 
-struct hash_t_hash {
-    size_t operator()(hash_t const& hentry) const
-    {
-        size_t const h1 ( std::hash<ah_table*>()(hentry.table_ptr) );
-        size_t const h2 ( std::hash<hash_object>()(hentry.hash_val) );
-        size_t const h3 ( std::hash<unsigned>()(hentry.offset) );
-        return h1 ^ (h3 ^ ((h2 << 1) << 1));
-    }
+struct hash_t_hash
+{
+  size_t operator()(hash_t const& hentry) const
+  {
+    size_t const h1 ( std::hash<ah_table*>()(hentry.table_ptr) );
+    size_t const h2 ( std::hash<hash_object>()(hentry.object_id) );
+    return h1 ^ ((h2 << 1) << 1);
+  }
 };
 
 typedef std::unordered_map<hash_t, uint32_t, hash_t_hash> timestamp_map_t;
@@ -85,44 +86,53 @@ typedef std::list<hash_t> entry_list_t;
 typedef std::pair<hash_t,uint32_t> hist_pair_t;
 typedef std::vector< hist_pair_t > hist_vec_t;
 
-struct wcount_t {
+struct wcount_t
+{
 
-    uint32_t excluded_windows;
+  uint32_t excluded_windows;
 
-    std::vector<uint32_t> common_windows;
+  std::vector<uint32_t> common_windows;
 
-    uint32_t all_windows;
+  uint32_t all_windows;
 
-    wcount_t(): excluded_windows(0), all_windows(0) {
-        common_windows = std::vector<uint32_t>(max_fpdist_ind+1);
+  wcount_t(): excluded_windows(0), all_windows(0)
+  {
+    common_windows = std::vector<uint32_t>(max_fpdist_ind+1);
+  }
+
+  wcount_t& operator+=(const wcount_t& rhs)
+  {
+    this->excluded_windows+=rhs.excluded_windows;
+    std::transform(rhs.common_windows.begin(),rhs.common_windows.end(), this->common_windows.begin(),this->common_windows.begin(),std::plus<uint32_t>());
+    this->all_windows += rhs.all_windows;
+    return *this;
+  }
+
+  friend std::ostream& operator << (std::ostream& out, const wcount_t& obj )
+  {
+    out << "excluded: " << obj.excluded_windows;
+    out << "\nsum of common: " << obj.all_windows;
+    out << "\ncommon:\n";
+    out << "size: "<<obj.common_windows.size() << "\n";
+
+    uint32_t check_sum=0;
+    for(const auto& count : obj.common_windows)
+    {
+      out << count << "\t";
+      check_sum+=count;
     }
 
-    wcount_t& operator+=(const wcount_t& rhs) {
-        this->excluded_windows+=rhs.excluded_windows;
-        std::transform(rhs.common_windows.begin(),rhs.common_windows.end(), this->common_windows.begin(),this->common_windows.begin(),std::plus<uint32_t>());
-        this->all_windows += rhs.all_windows;
-        return *this;
-    }
-
-    friend std::ostream& operator << (std::ostream& out, const wcount_t& obj ) {
-        out << "excluded: " << obj.excluded_windows;
-        out << "\nsum of common: " << obj.all_windows;
-        out << "\ncommon:\n";
-        out << "size: "<<obj.common_windows.size() << "\n";
-
-        uint32_t check_sum=0;
-        for(const auto& count : obj.common_windows) {
-            out << count << "\t";
-            check_sum+=count;
-        }
-
-        assert(obj.all_windows==check_sum);
+    assert(obj.all_windows==check_sum);
 
 
-        out << "\n----------------------------\n";
+    out << "\n----------------------------\n";
 
-        return out;
-    }
+    return out;
+  }
+
+  uint32_t affinity(){
+    return std::accumulate(common_windows.begin(),common_windows.end());
+  }
 
 };
 
@@ -130,24 +140,28 @@ typedef std::unordered_map <const hash_t, wcount_t, hash_t_hash > wcount_map_t;
 
 
 
-struct all_wcount_t {
-    uint32_t potential_windows;
-    wcount_map_t wcount_map;
-    all_wcount_t(uint32_t _pw): potential_windows(_pw) {}
-    all_wcount_t(): potential_windows(0) {
-        wcount_map = wcount_map_t();
-    }
+struct all_wcount_t
+{
+  uint32_t potential_windows;
+  wcount_map_t wcount_map;
+  all_wcount_t(uint32_t _pw): potential_windows(_pw) {}
+  all_wcount_t(): potential_windows(0)
+  {
+    wcount_map = wcount_map_t();
+  }
 
-    friend std::ostream& operator << (std::ostream& out, const all_wcount_t& obj) {
-        out << "potential windows: " << obj.potential_windows;
-        out << "\nwindow count map:\n";
+  friend std::ostream& operator << (std::ostream& out, const all_wcount_t& obj)
+  {
+    out << "potential windows: " << obj.potential_windows;
+    out << "\nwindow count map:\n";
 
-        for(const auto& wcount_pair : obj.wcount_map) {
-            out << wcount_pair.first << "\n";
-            out << wcount_pair.second << "\n";
-        }
-        return out;
+    for(const auto& wcount_pair : obj.wcount_map)
+    {
+      out << wcount_pair.first << "\n";
+      out << wcount_pair.second << "\n";
     }
+    return out;
+  }
 
 
 
@@ -155,94 +169,109 @@ struct all_wcount_t {
 
 typedef std::unordered_map <const hash_t, all_wcount_t , hash_t_hash > all_wcount_map_t;
 
-std::ostream& operator << (std::ostream& out, const all_wcount_map_t& m) {
-    for (const auto& all_wcount_pair: m) {
-        out << "************************\n";
-        out << all_wcount_pair.first << "\n";
-        out << all_wcount_pair.second << "\n";
-    }
-    return out;
+std::ostream& operator << (std::ostream& out, const all_wcount_map_t& m)
+{
+  for (const auto& all_wcount_pair: m)
+  {
+    out << "************************\n";
+    out << all_wcount_pair.first << "\n";
+    out << all_wcount_pair.second << "\n";
+  }
+  return out;
 }
 
-struct window_t {
-    entry_vec_t owners;
-    wsize_t wsize;
-    wsize_t capacity;
-    uint32_t start;
-    //entry_list_t partial_entry_list;
+struct window_t
+{
+  entry_vec_t owners;
+  wsize_t wsize;
+  wsize_t capacity;
+  uint32_t start;
+  //entry_list_t partial_entry_list;
 
-    window_t():wsize(0) {
-        owners.reserve(analysis_set_size);
+  window_t():wsize(0)
+  {
+    owners.reserve(analysis_set_size);
+  }
+
+  window_t(const hash_t& entry):wsize(1), capacity(1), start(timestamp)
+  {
+    owners.reserve(analysis_set_size);
+    owners.push_back(entry);
+  }
+
+  ~window_t()
+  {
+    owners.clear();
+  }
+
+  void push_front(const hash_t& entry)
+  {
+    wsize++;
+  }
+
+
+  void erase(const entry_list_t::iterator& it)
+  {
+    //partial_entry_list.erase(it);
+    wsize--;
+  }
+
+  bool empty()
+  {
+    return wsize == 0;
+  }
+
+  void merge_owners(const window_t& other)
+  {
+    entry_vec_t::iterator eit_begin = owners.begin();
+    entry_vec_t::iterator eit_end = owners.end();
+    for(const auto &e: other.owners)
+    {
+      if(std::find(eit_begin,eit_end,e)==eit_end)
+        owners.push_back(e);
     }
-
-    window_t(const hash_t& entry):wsize(1), capacity(1), start(timestamp) {
-        owners.reserve(analysis_set_size);
-        owners.push_back(entry);
-    }
-
-    ~window_t() {
-        owners.clear();
-    }
-
-    void push_front(const hash_t& entry) {
-        wsize++;
-    }
+  }
 
 
-    void erase(const entry_list_t::iterator& it) {
-        //partial_entry_list.erase(it);
-        wsize--;
-    }
+  friend std::ostream& operator << (std::ostream& out, const window_t& obj)
+  {
+    out << "entries: ";
+    /*for(const auto &entry: obj.partial_entry_list)
+      out << entry << " ";
+    		*/
+    out << "\nowners: " ;
 
-    bool empty() {
-        return wsize == 0;
-    }
+    for(const auto& entry: obj.owners)
+      out << entry << " ";
 
-    void merge_owners(const window_t& other) {
-        entry_vec_t::iterator eit_begin = owners.begin();
-        entry_vec_t::iterator eit_end = owners.end();
-        for(const auto &e: other.owners) {
-            if(std::find(eit_begin,eit_end,e)==eit_end)
-                owners.push_back(e);
-        }
-    }
-
-
-    friend std::ostream& operator << (std::ostream& out, const window_t& obj) {
-        out << "entries: ";
-        /*for(const auto &entry: obj.partial_entry_list)
-          out << entry << " ";
-        		*/
-        out << "\nowners: " ;
-
-        for(const auto& entry: obj.owners)
-            out << entry << " ";
-
-        out << "\n:size: " << obj.wsize << "\n";
-        return out;
-    }
+    out << "\n:size: " << obj.wsize << "\n";
+    return out;
+  }
 
 };
 
 typedef std::list<window_t> window_list_t;
 
 
-std::ostream& operator << (std::ostream& out, const window_list_t& wlist) {
-    for (const auto& window: wlist) {
-        out << window << "\n";
-    }
-    return out;
+std::ostream& operator << (std::ostream& out, const window_list_t& wlist)
+{
+  for (const auto& window: wlist)
+  {
+    out << window << "\n";
+  }
+  return out;
 }
 
 
-struct wlist_it_pair_t {
-    window_list_t::iterator window_it;
-    entry_list_t::iterator entry_it;
+struct wlist_it_pair_t
+{
+  window_list_t::iterator window_it;
+  entry_list_t::iterator entry_it;
 
-    wlist_it_pair_t() {}
+  wlist_it_pair_t() {}
 
-    wlist_it_pair_t(const window_list_t::iterator& wit, const entry_list_t::iterator& eit)
-        :window_it (wit), entry_it (eit) {}
+  wlist_it_pair_t(const window_list_t::iterator& wit, const entry_list_t::iterator& eit)
+    :window_it (wit), entry_it (eit) {}
 };
 
 typedef std::unordered_map <const hash_t, window_list_t::iterator, hash_t_hash > window_iterator_map_t;
@@ -252,25 +281,48 @@ typedef std::unordered_map <const hash_t, entry_list_t::iterator, hash_t_hash > 
 
 
 
-
-hist_pair_t get_hist_entry (const std::pair<const hash_t, wcount_t> &p) {
-    return hist_pair_t (p.first,p.second.all_windows);
+hist_pair_t get_hist_entry (const std::pair<const hash_t, wcount_t> &p)
+{
+  return hist_pair_t (p.first,p.second.all_windows);
 }
 
 /*
  * We need to sort in descending order. Thus, we flip the order
  */
-bool hist_pair_cmp (const hist_pair_t &e1, const hist_pair_t &e2) {
-    return (e1.second > e2.second);
+bool hist_pair_cmp (const hist_pair_t &e1, const hist_pair_t &e2)
+{
+  return (e1.second > e2.second);
 }
 
-uint32_t hist_pair_add (const uint32_t psum, const hist_pair_t &e) {
-    return psum+e.second;
+uint32_t hist_pair_add (const uint32_t psum, const hist_pair_t &e)
+{
+  return psum+e.second;
 }
 
+struct wcount_pair_t {
+  hash_t lentry, rentry;
+  wcount_t wcount;
+  uint32_t affinity;
 
+  wcount_pair_t(hash_t le, hash_t re, uint32_t affinity): lentry(le), rentry(re), affinity(affinity){}
+
+  bool operator < (const wcount_pair_t wcount_pair) const {
+    return (affinity < wcount_pair.affinity);
+  }
+
+  friend std::ostream& operator << (std::ostream& out, const wcount_pair_t wcount_pair)
+  {
+    out << "[" << wcount_pair.lentry << "," << wcount_pair.rentry << "]:" << wcount_pair.affinity;
+    return out;
+  }
+
+};
+
+
+void find_affinity_layout();
 void affinity_at_exit_handler();
 extern "C" void init_affinity_analysis();
 void update_stage_affinity(const hash_t&, const window_list_t::iterator&);
 extern "C" void trace_hash_access(ah_table *, hash_object, unsigned, bool);
 extern "C" void remove_table_analysis(ah_table *);
+extern "C" void remove_entry(ah_table *, int object_id);
